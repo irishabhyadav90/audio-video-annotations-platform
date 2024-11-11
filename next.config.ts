14646e961bcaba01862c2626bb3099edd0a393a2
Rishabh Raena <rishabh@raenabeauty.com>
2024-11-11 15:19:45 +0400

    fix: Routing structure and duplication annotation

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/upload',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
